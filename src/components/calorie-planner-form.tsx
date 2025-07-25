33f38fca797a3f7a52fcdf5f2424a5e70d22f8f4
Atharv Patil <atharvpatil269@gmail.com>
2025-07-26 00:11:43 +0530

    chore: add placeholder components for missing calculators to fix build

---
+++ src/components/calorie-planner-form.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function RetirementPlannerForm() {
+  return (
+    <div className="p-4 border rounded bg-yellow-50 text-yellow-800">
+      Placeholder: RetirementPlannerForm component is missing. Please implement this form.
+    </div>
+  );
+} 
